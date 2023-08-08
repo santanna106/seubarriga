a4dd60ef342bbb6208ca63a0b90184b9227c4f44
Gabriel Andrade <gabriel.santana@gmail.com>
2023-08-08 05:23:35 -0300

    Create container postgresql with docker-compose

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM node:latest
+
+WORKDIR /usr/app
+
+COPY package.json /usr/app/
+
+RUN yarn install
+
+COPY . . 
+
+EXPOSE 3000
+
+
+CMD ["yarn", "start"]
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -13,22 +13,7 @@ router.get('/',(req: Request, res: Response) => {
     res.status(200).json({msg:'server ok'})
 })
 
-router.get('/users',(req: Request, res: Response) => {
-    const users = [ 
-        {name: 'John Doe',email:'john@gmail.com'}
-    ]
-    res.status(200).json(users);
-});
 
-
-
-router.post("/users", (req: Request, res: Response, next: Function): void =>{
-    if(!req.body.name){
-       res.status(400).json('You need to pass  name')
-    }
-    res.status(201).json(req.body);
-});
-       
 
 /*
 router.post("/register", (req: Request, res: Response, next: Function): void =>{
